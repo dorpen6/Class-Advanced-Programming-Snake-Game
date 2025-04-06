a34c22175e7899a404d3f67d05fb861f437b2339
Ali Valishov <108894573+dorpen6@users.noreply.github.com>
2025-04-06 02:34:40 +0200

    defining classes & objects

--- main.py
+++ main.py
@@ -2,3 +2,21 @@ from tkinter import *
 import random
 
 
+# classes & objects
+
+class Snake:
+    pass
+
+class Food:
+    pass
+
+class next_turn():
+    pass
+
+def change_direction(new_direciton):
+    pass
+
+def game_over():
+    pass
+
+
